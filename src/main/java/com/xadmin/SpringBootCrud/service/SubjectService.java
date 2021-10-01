29e21fdb58dfdd27651fc33f3cf6f7a24764a827
umar <umar545f@gmail.com>
2021-10-01 15:57:44 +0530

    Commit

---
+++ src/main/java/com/xadmin/SpringBootCrud/service/SubjectService.java
@@ -0,0 +1,35 @@
+package com.xadmin.SpringBootCrud.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.xadmin.SpringBootCrud.bean.Subject;
+import com.xadmin.SpringBootCrud.repository.SubjectRepository;
+
+@Service
+public class SubjectService {
+	@Autowired
+	public SubjectRepository subjectRepo;
+	public List<Subject> getAllSubjects()
+	{
+		List<Subject> subjects=new ArrayList<>();
+		subjectRepo.findAll().forEach(subjects::add);
+		return subjects;
+	}
+	public void addSubject(Subject subject) {
+		// TODO Auto-generated method stub
+		subjectRepo.save(subject);
+		
+	}
+	public void updateSubject(String id, Subject subject) {
+		subjectRepo.save(subject);	
+	}
+	public void deleteSubject(String id) {
+		// TODO Auto-generated method stub
+		subjectRepo.deleteById(id);
+	}
+	
+}
